a0bd42fee0e6ebee359032e5db01ff960ba2b3de
shaw342 <baruashawan41@gmail.com>
2023-05-12 15:10:24 +0200

    test4

--- main.c
+++ main.c
@@ -1,37 +1,43 @@
 #include "monty.h"
 int error = 0;
+/**
+ *main - the main fonction
+ *@argv: tab of arguments
+ *@argc: number of arguments
+ *Return: EXIT_FAILURE or EXIT_SUCCESS
+ */
 int main(int argc, char **argv)
 {
-	FILE *fp;
+	FILE *ptr;
 	char *line = NULL,*token = NULL;
 	size_t size = 0;
-	unsigned int line_number = 0;
 	stack_t *stack = NULL;
+	unsigned int line_number = 0;
 
 	if (argc != 2) 
 	{
-		perror("USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	fp = fopen(argv[1],"r");
+	ptr = fopen(argv[1],"r");
 
-	if (fp == NULL)
+	if (ptr == NULL)
 	{
-		printf("Error: Can't open file %s\n",argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n",argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while(getline(&line,&size,fp) != -1 && error != 1) 
+	while(getline(&line,&size,ptr) != -1 && error != 1) 
 	{
 		line_number++;
-		token = strtok(line,"\n\t");
+		token = strtok(line,"\n\t ");
 		if (token ==  NULL || strncmp(token,"#",1) == 0)
 			continue;
 
 		if (strcmp(token,"push") ==  0)
 		{
 
-			token = strtok(NULL,"\n\t");
+			token = strtok(NULL,"\n\t ");
 			__push(token,&stack, line_number);
 		}
 		else
@@ -40,9 +46,9 @@ int main(int argc, char **argv)
 		}
 
 	}
-	fclose(fp);
+	free_all(stack, line, ptr);
 	if (error == 1)
 		exit(EXIT_FAILURE);
 
-	return (0);
+	return (EXIT_SUCCESS);
 }
