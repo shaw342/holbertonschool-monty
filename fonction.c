47471f82dc7c28a942d368e278ef087e3b24ea5f
shaw342 <baruashawan41@gmail.com>
2023-05-13 11:40:56 +0200

    add fonction.c

--- fonction.c
+++ fonction.c
@@ -61,3 +61,13 @@ void __nop(stack_t **stack,unsigned int line_number)
 	(void) **stack;
 	(void) line_number;
 }
+void __pint(stack_t **stack,unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr,"L%d: can't pint, stack empty\n",line_number);
+		error = 1;
+		exit(EXIT_FAILURE);
+	}
+	printf("%d",(*stack)->n);
+}
